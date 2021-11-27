b012570fe3b7621ab2220c2675104cd432354cab
Teressa Sung <teressas.hawaii@gmail.com>
2021-11-27 14:38:24 -0800

    initial_commit

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Teressa Sung");
+        System.out.println("29");
+        System.out.println("Honolulu, HI");
+    }
+}
