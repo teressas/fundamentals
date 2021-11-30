6eb64bd25a57a957e99e5dcfb0481a391a002f7f
Teressa Sung <teressas.hawaii@gmail.com>
2021-11-29 22:06:58 -0800

    Master

---
+++ puzzling/Puzzling.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Puzzling{
+    
+    public int getTenRolls() {
+
+        ArrayList<Integer> myArray = new ArrayList<Integer>();
+        Random randMachine = new Random();
+
+    }
+}
+    
+// To use methods from the Random library you will need to create an instance of Random
+// From there you can use any of the methods listed in the documentation. For example:
+
